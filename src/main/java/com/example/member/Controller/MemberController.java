674e9b25dbea7561cf52e0b7b768a6bc181a4363
GalaxyOpen <keehong11@naver.com>
2023-06-02 10:44:21 +0900

    2023.06.02 update 진행 완료

--- src/main/java/com/example/member/Controller/MemberController.java
+++ src/main/java/com/example/member/Controller/MemberController.java
@@ -51,11 +51,6 @@ public class MemberController {
         }else{
             return "/memberPages/memberLogin";
         }
-    }@GetMapping("/member/{id}")
-    public String findById(@PathVariable Long id, Model model){
-        MemberDTO memberDTO = memberService.findById(id);
-        model.addAttribute("member",memberDTO);
-        return "/memberPages/memberDetail";
     }
 
     @PostMapping("/member/login/axios")
@@ -64,6 +59,22 @@ public class MemberController {
         session.setAttribute("loginEmail", memberDTO.getMemberEmail());
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping("/member/myPage")
+    public String myPage(){
+        return "/memberPages/memberMain";
+    }
+
+//    @GetMapping("/member/{id}")
+//    public String findById(@PathVariable Long id, Model model){
+//        MemberDTO memberDTO = memberService.findById(id);
+//        model.addAttribute("member",memberDTO);
+//        return "/memberPages/memberDetail";
+//    }
+
+
+
+
     @GetMapping("/member/axios/{id}")
     public ResponseEntity detailAxios(@PathVariable Long id) throws Exception{
         MemberDTO memberDTO = memberService.findById(id);
@@ -74,11 +85,25 @@ public class MemberController {
         memberService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model){
+        String loginEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
+        model.addAttribute("member", memberDTO);
+//        model.addAttribute("member", memberService.findByMemberEmail(loginEmail));
+        // 이렇게 한줄로 쓰기도 함. 더 극단적인 케이스는 String loginEmail 선언 없이도 하지만 글쎄...? 하지만 읽을순 있어야함.
+        return "memberPages/memberUpdate";
+    }
     @PutMapping("/member/{id}")
-    public ResponseEntity update(@PathVariable Long id,
-                                @RequestBody MemberDTO memberDTO){
-        memberService.update(id,memberDTO);
+    public ResponseEntity update(@RequestBody MemberDTO memberDTO){
+        memberService.update(memberDTO);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping("/member/{id}")
+    public String detail(@PathVariable Long id, Model model){
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member",memberDTO);
+        return "/memberPages/memberDetail";
     }
 
 }
--- src/main/java/com/example/member/Service/MemberService.java
+++ src/main/java/com/example/member/Service/MemberService.java
@@ -55,18 +55,17 @@ public class MemberService {
 //            return null;
 //        }
 //    }
-    public MemberDTO findById(Long id){
-//    MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(()->new NoSuchElementException());
-//    return MemberDTO.toDTO(memberEntity);
-      Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
-      if(optionalMemberEntity.isPresent()) {
-          MemberEntity memberEntity = optionalMemberEntity.get();
-          return MemberDTO.toDTO(memberEntity);
-      }else{
-          return null;
-      }
-    }
-
+public MemberDTO findById(Long id) {
+    MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+    return MemberDTO.toDTO(memberEntity);
+//        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+//        if (optionalMemberEntity.isPresent()) {
+//            MemberEntity memberEntity = optionalMemberEntity.get();
+//            return MemberDTO.toDTO(memberEntity);
+//        } else {
+//            return null;
+//        }
+}
     public void loginAxios(MemberDTO memberDTO) {
         memberRepository.findByMemberEmailAndMemberPassword(memberDTO.getMemberEmail(), memberDTO.getMemberPassword())
                         .orElseThrow(() ->new NoSuchElementException("이메일 또는 비밀번호가 틀립니다"));
@@ -74,26 +73,27 @@ public class MemberService {
                // 보통 이런 경우 . 이 어딨는지, "나 '는 어딨는지 확인하여야 코드 확인이 편하다
      }
 
+
     public void delete(Long id) {
         memberRepository.deleteById(id);
     }
+    //이메일 중복체크 때에도 쓸 수 있음.
+    public MemberDTO findByMemberEmail(String loginEmail) {
+        // 조회를 하면서 없으면 예외처리, 있으면 MemberEntity 리턴
+        MemberEntity memberEntity = memberRepository.findByMemberEmail(loginEmail).orElseThrow(()-> new NoSuchElementException("뭔가 잘못됬습니다."));
+        //있는 경우 DTO로 변환하여 컨트롤러로 리턴
+        return MemberDTO.toDTO(memberEntity);
+    }
     public MemberDTO detailAxios(Long id) {
         MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(() -> new NoSuchElementException("회원이 없습니다"));
-        return MemberDTO.toDTO(memberEntity);}
+        return MemberDTO.toDTO(memberEntity);
+    }
 
-    public void update(Long id, MemberDTO memberDTO) {
-        Optional<MemberEntity> memberEntityOptional = memberRepository.findById(id);
-
-        if(memberEntityOptional.isEmpty()){
-            throw new EntityNotFoundException("회원이 없습니다.");
-        }
-        MemberEntity memberEntity = memberEntityOptional.get();
-
-        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
-        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
-        memberEntity.setMemberName(memberDTO.getMemberName());
-        memberEntity.setMemberMobile(memberDTO.getMemberMobile());
-
+    public void update(MemberDTO memberDTO) {
+        MemberEntity memberEntity = MemberEntity.toUpdateEntity(memberDTO);
         memberRepository.save(memberEntity);
     }
+
+
+
 }
